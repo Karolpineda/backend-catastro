69cb4be7314955a5d32e5b5a34bd44961d7d0790
Karolpineda <karolpineda@hotmail.com>
2025-08-21 15:41:52 -0500

    feat: :sparkles: Init commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ZonaModule } from './zona/zona.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 5432,
+      host: 'localhost',
+      username: 'postgres',
+      password: 'postgres',
+      database: 'catastro_tp',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+      
+    ZonaModule, ],
+  controllers: [],
+
+})
+export class AppModule {}
