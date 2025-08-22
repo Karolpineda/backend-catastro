7212a044e6d89ac0aea846bc313fa542dbcd0f81
Karolpineda <karolpineda@hotmail.com>
2025-08-22 08:06:33 -0500

    feat: :construction_worker: Update of module estado accidente and incidente

---
+++ src/estado_acc_inc/dto/create-estado_acc_inc.dto.ts
@@ -0,0 +1,6 @@
+export class CreateEstadoAccIncDto{
+      
+        nombre_estado_acc_inc:string
+        descrip_estado_acc_inc: string
+    
+}
---
+++ src/estado_acc_inc/dto/update-estado_acc_inc.dto.ts
@@ -0,0 +1,6 @@
+export class UpdateEstadoAccIncDto{
+      
+        nombre_estado_acc_inc?:string
+        descrip_estado_acc_inc?: string
+    
+}
