a2ea8e8b13c6fe3f91f378749a0f5e52dfdde25d
Karolpineda <karolpineda@hotmail.com>
2025-08-26 15:46:22 -0500

    feat: :truck: Update rute of modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ZonaModule } from './zona/zona.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { EstadoAccIncModule } from './estado_acc_inc/estado_acc_inc.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { RolModule } from './rol/rol.module';
+import { SistemaModule } from './sistema/sistema.module';
+import { DependenciaModule } from './dependencia/dependencia.module';
+import { ClasifCatastralModule } from './clasif_catastral/clasif_catastral.module';
+import { CategoriaModule } from './categoria/categoria.module';
 
 
 @Module({
@@ -18,7 +24,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       migrations: [__dirname + '/migrations/*{.ts,.js}'],
     }),
       
-    ZonaModule, ],
+    ZonaModule, UsuarioModule, EstadoAccIncModule, RolModule, SistemaModule, DependenciaModule, ClasifCatastralModule, CategoriaModule],
   controllers: [],
 
 })
