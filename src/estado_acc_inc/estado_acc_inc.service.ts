e4f5086f042a09f2ad215497016159fc4543bf01
Karolpineda <karolpineda@hotmail.com>
2025-08-28 13:44:29 -0500

    feat: :sparkles: Update syncronice of estado service

--- src/estado_acc_inc/estado_acc_inc.service.ts
+++ src/estado_acc_inc/estado_acc_inc.service.ts
@@ -10,23 +10,25 @@ export class EstadoAccIncService {
 
     constructor(@InjectRepository(Estado_acc_inc) private estadoAccIncRepository: Repository<Estado_acc_inc>){}
 
-    createEstadoAccInc(estadoAccInc: CreateEstadoAccIncDto){
-        const newEstadoAccInc = this.estadoAccIncRepository.create(estadoAccInc)
-        return this.estadoAccIncRepository.save(newEstadoAccInc)
-
-    }
-    getAllEstadoAccInc(){
-        return this.estadoAccIncRepository.find();
+    async createEstadoAccInc(estadoAccInc: CreateEstadoAccIncDto){
+        const newEstadoAccInc = await this.estadoAccIncRepository.create(estadoAccInc)
+        return await this.estadoAccIncRepository.save(newEstadoAccInc)
     }
 
-    getEstadoAccInc(nombre_estado_acc_inc: string){
-        return this.estadoAccIncRepository.findOne({ where: { nombre_estado_acc_inc } });
+    async getAllEstadoAccInc(){
+        return  this.estadoAccIncRepository.find();
     }
 
-    deleteEstadoAccInc(id_estado_acc_inc:number){
-        return this.estadoAccIncRepository.delete([id_estado_acc_inc]);
+    async getEstadoAccInc(nombre_estado_acc_inc: string){
+        return  this.estadoAccIncRepository.findOne({ where: { nombre_estado_acc_inc } });
     }
-    updateEstadoAccInc(id_estado_acc_inc:number,estadoAccInc: UpdateEstadoAccIncDto){
-        return this.estadoAccIncRepository.update(id_estado_acc_inc, estadoAccInc);
+
+    async deleteEstadoAccInc(id_estado_acc_inc:number){
+        return  this.estadoAccIncRepository.delete([id_estado_acc_inc]);
     }
+
+    async updateEstadoAccInc(id_estado_acc_inc:number,estadoAccInc: UpdateEstadoAccIncDto){
+        return  this.estadoAccIncRepository.update(id_estado_acc_inc, estadoAccInc);
+    }
+
 }
