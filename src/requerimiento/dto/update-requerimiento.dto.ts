04ed657321eaf437e61fc4b1dd43df8986422630
Karolpineda <karolpineda@hotmail.com>
2025-10-03 14:30:31 -0500

    feat: :sparkles: Update relatios of versionamiento

--- src/requerimiento/dto/update-requerimiento.dto.ts
+++ src/requerimiento/dto/update-requerimiento.dto.ts
@@ -1,4 +1,45 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateRequerimientoDto } from './create-requerimiento.dto';
+// update-requerimiento.dto.ts
+import { IsString, IsInt, IsOptional } from 'class-validator';
 
-export class UpdateRequerimientoDto extends PartialType(CreateRequerimientoDto) {}
+export class UpdateRequerimientoDto {
+  @IsOptional()
+  @IsInt()
+  id_estado_requerimiento?: number;
+
+  @IsOptional()
+  @IsInt()
+  id_rol_usuario?: number;
+
+  @IsOptional()
+  @IsInt()
+  id_categoria?: number;
+
+  @IsOptional()
+  @IsInt()
+  id_sistema?: number;
+
+  @IsOptional()
+  @IsString()
+  no_requerimiento?: string;
+
+  @IsOptional()
+  @IsString()
+  documento?: string;
+
+  @IsOptional()
+  @IsString()
+  tema?: string;
+
+  @IsOptional()
+  @IsString()
+  descripcion?: string;
+
+  @IsOptional()
+  @IsString()
+  fase?: string;
+
+  // 🆕 Versionamiento como objeto simple - SIN VALIDACIÓN COMPLEJA
+  @IsOptional()
+  versionamiento?: any;
+  
+}
