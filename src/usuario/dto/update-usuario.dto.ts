a7b6c100ce45324f3cbc11aee88683dfbff4f996
Karolpineda <102493529+Karolpineda@users.noreply.github.com>
2025-10-18 12:54:20 -0500

    Add validation decorators to UpdateUsuarioDto fields

--- src/usuario/dto/update-usuario.dto.ts
+++ src/usuario/dto/update-usuario.dto.ts
@@ -1,7 +1,21 @@
 export class UpdateUsuarioDto {
+    @IsString()
+    @IsOptional()
     cedula_usuario?: string
+
+    @IsString()
+    @IsOptional()
     apellidos_usuario?: string
+
+    @IsString()
+    @IsOptional()
     nombre_usuario?: string
+
+    @IsString()
+    @IsOptional()
     correo_usuario?: string
+
+    @IsString()
+    @IsOptional()
     contrasenia_usuario?: string
 }
