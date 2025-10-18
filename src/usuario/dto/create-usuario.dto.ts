f33f35c0367ec4dbe4a41d94c653c6c86543308c
Karolpineda <102493529+Karolpineda@users.noreply.github.com>
2025-10-18 12:52:50 -0500

    Add validation decorators to CreateUsuarioDto

--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,7 +1,20 @@
 export class CreateUsuarioDto {
+    @IsString()
+    @IsOptional()
     cedula_usuario: string;
+
+    @IsString()
+    @IsOptional()
     apellidos_usuario: string;
+
+    @IsString()
+    @IsOptional()
     nombre_usuario: string;
+    @IsString()
+    @IsOptional()
     correo_usuario: string;
+
+    @IsString()
+    @IsOptional()
     contrasenia_usuario: string;
 }
