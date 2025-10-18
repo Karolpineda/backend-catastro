c6902173314d4a4809a3bc037523d4db6ccb1f25
Karolpineda <karolpineda@hotmail.com>
2025-10-17 20:27:08 -0500

    feat: :sparkles: Add type of atributes

--- src/versionamiento/entities/versionamiento.entity.ts
+++ src/versionamiento/entities/versionamiento.entity.ts
@@ -1,5 +1,7 @@
 import { RequerimientoVersion } from "src/requerimiento-version/entities/requerimiento-version.entity";
 import { Column, CreateDateColumn, Entity,OneToMany,PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { TestVersion } from "src/test-version/entities/test-version.entity";
+
 
 @Entity()
 export class Versionamiento {
@@ -7,23 +9,23 @@ export class Versionamiento {
      @PrimaryGeneratedColumn()
     id_version: number
 
-    @Column({ nullable: true})
-    oficioenviodmi:string
+    @Column({ type: 'varchar', nullable: true})
+    oficioenviodmi:string | null
 
     @Column({ type: 'date', nullable: true })
     fechaenvioreq: Date | null;
 
-     @Column({nullable: true})
-    ofi_desp_pt:string
+     @Column({type: 'varchar', nullable: true})
+    ofi_desp_pt:string | null
 
     @Column({ type: 'date', nullable: true })
     fech_desp_pt: Date | null;
 
-    @Column({nullable: true})
+    @Column({type: 'int', nullable: true})
     num_version: number
 
     @Column({ type: 'text', nullable: true })
-    obs_version: string;
+    obs_version: string | null;
     
     @CreateDateColumn()
     createdAt: Date;
@@ -33,4 +35,7 @@ export class Versionamiento {
 
     @OneToMany(() => RequerimientoVersion, (reqVersion) => reqVersion.versionamiento)
     requerimientoVersiones: RequerimientoVersion[];
+
+    @OneToMany(() => TestVersion, (testVersion) => testVersion.versionamiento)
+    test_versions: TestVersion[];
 }
